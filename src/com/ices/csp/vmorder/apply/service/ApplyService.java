ce223fcbf32bfae8e25fb40748755c87dd4c2a9a
yangengzhe <emailvip@vip.qq.com>
2016-04-26 09:27:49 +0800

    delete src

--- src/com/ices/csp/vmorder/apply/service/ApplyService.java
+++
@@ -1,7 +0,0 @@
-package com.ices.csp.vmorder.apply.service;
-
-import com.ices.csp.vmorder.apply.dto.ApplyDto;
-
-public interface ApplyService {
-	public void addorder(ApplyDto applyDto) throws Exception;
-}
